d21696542555b292f537f427a29ab07365b46796
coding-to-music <connors.tom@gmail.com>
2022-04-01 05:46:16 +0000

    Heroku

--- oldclient/src/services/uploadWs.js
+++
@@ -1,8 +0,0 @@
-import { apiServer } from "../api/ServerAPI";
-import { successStatus, internalServerError } from "../utils/clear-res";
-
-export const uploadWs = (file) =>
-  apiServer
-    .post("/upload", file)
-    .then(successStatus)
-    .catch(internalServerError);
