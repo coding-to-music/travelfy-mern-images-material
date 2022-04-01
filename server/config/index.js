7e8a78c14e746839ad3df254909b14aeaa9c5d27
coding-to-music <connors.tom@gmail.com>
2022-04-01 05:00:23 +0000

    Heroku

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,4 @@
-import path from "path";
-import express from "express";
+const path = require("path");
 
 // We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
