b9c5f90a8fc8bc3b25e67683fbae43127cc71244
coding-to-music <connors.tom@gmail.com>
2022-04-01 08:08:37 +0000

    Heroku

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
