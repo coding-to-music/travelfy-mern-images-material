c20f5b4ba86c99661b61614b846bba4ddd74f880
coding-to-music <connors.tom@gmail.com>
2022-04-02 16:59:38 +0000

    Heroku

--- react-ui/src/components/Map/index.jsx
+++ react-ui/src/components/Map/index.jsx
@@ -2,13 +2,21 @@ import "./Map.css";
 import GoogleMapReact from "google-map-react";
 import PlaceMarker from "../PlaceMarker";
 
+console.log(
+  "REACT_APP_GOOGLE_MAP_API_KEY",
+  process.env.REACT_APP_GOOGLE_MAP_API_KEY
+);
+
 const Map = ({ places, coordinates, setCoordinates, setLimits, zoom }) => {
   return (
     <>
       <div className="map-wrapper">
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY, libraries:['places','geometry','drawing'] }}
-          // Map Style (Google Developers Plattaform)
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+            libraries: ["places", "geometry", "drawing"],
+          }}
+          // Map Style (Google Developers Platform)
           mapId={process.env.REACT_MAP_GOOGLE_MAP_ID}
           center={coordinates}
           defaultZoom={zoom}
