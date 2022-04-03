13b7bff5957fcfe54b8d0b5ed7126ab293c54abe
coding-to-music <connors.tom@gmail.com>
2022-04-03 03:51:51 +0000

    Heroku

--- server/config/index.js
+++ server/config/index.js
@@ -18,6 +18,21 @@ const cors = require("cors");
 // dotenv config
 require("dotenv").config();
 
+console.log(
+  "NODE_ENV",
+  process.env.NODE_ENV
+);
+
+console.log(
+  "REACT_MAP_GOOGLE_MAP_ID",
+  process.env.REACT_MAP_GOOGLE_MAP_ID
+);
+
+console.log(
+  "REACT_APP_GOOGLE_MAP_API_KEY",
+  process.env.REACT_APP_GOOGLE_MAP_API_KEY
+);
+
 // app and port config
 const app = express();
 const port = process.env.PORT || 4000;
